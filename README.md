6e512234e231c3afe8c9668a076f6b58ece29a7e
Avneesh <avneesh_18@hotmail.com>
2014-07-03 01:07:14 +0530

    Added code for getting getting data from the rotten tomato API

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ MovieRating
 ===========
 
 Little Python nautilus script for pulling movie rating from rotten tomatoes
+and displayed using notify-send notification displayer
--- movieRating.py
+++ movieRating.py
@@ -9,10 +9,12 @@ class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
         pass
     def execute(self,menu,files):
-        print files[0].get_name()
-        cmd = "notify-send '"+ files.name +"'"
-        os.system(cmd)
-	pass
+        if len(files) == 1:
+          print files[0].get_name()
+          cmd = "notify-send '"+ files[0].get_name() +"'"
+          os.system(cmd)
+	else:
+          pass
     def get_file_items(self, window, files):
         top_menuitem = Nautilus.MenuItem(name='MovieRating', 
                                          label='Movie Actions', 
@@ -21,3 +23,36 @@ class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
         
  	top_menuitem.connect('activate',self.execute,files)       
         return top_menuitem,
+
+
+
+
+
+class rottenTom:
+    """A very basic wrapper for the rottentomatoes api"""
+
+    cache_count = 20
+    url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?' + \
+          'apikey=ufykyv9d94bcxfrjaw6xacqr&q={}&page_limit={}'
+
+    def __init__(self):
+
+        self.movie_cache = {} # caches data returned by api
+        self.movies_in_cache = {} # caches movie names and number of calls since last used
+
+  
+
+    def getMovie(self,movie_name,page_limit = 1):
+
+    	movie_name = movie_name.lower().strip()
+    	movie_format = movie_name.replace(u" ", u"+")
+    	"""
+    	Get a list of movies in theaters. 
+    	"""
+    	# in the url we need to use '+' instead of spaces
+    	movie_format = movie_name.replace(u" ", u"+")
+    	query = self.url.format(movie_format, page_limit)
+    	result_data = urllib2.urlopen(query)
+    	results = json.loads(result_data.read())
+       
+    	return json.dumps(results)
