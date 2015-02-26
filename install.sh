fc89bb1aa479b1dc9a57838435a65a13a1388677
Avneesh <avneesh_18@hotmail.com>
2015-02-27 00:43:56 +0530

    Adding file for installing file installing file

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+dpkg -s python-nautilus >/dev/null 2>&1 &&{
+     echo "Python Nautilus found"
+     echo "Checking for python-nautilus folders"
+
+     if [! -d ~/.local/share/nautilus-python ]; then
+        mkdir ~/.local/share/nautilus-python
+     fi
+    
+     if [! -d ~/.local/share/nautilus-python/extensions]; then
+          mkdir ~/.local/share/nautilus-python/extensions
+     fi
+     
+     echo "Copying files"     
+
+     cp imdb.py ~/.local/share/nautilus-python/extensions
+     cp MessageMaker.py ~/.local/share/nautilus-python/extensions
+     cp movieRating.py ~/.local/share/nautilus-python/extensions
+     cp notify.py ~/.local/share/nautilus-python/extensions   
+
+     echo "Restarting Nautilus"
+
+     nautilus -q
+     
+     echo "Done"
+
+   } ||{
+    echo "Python-Nautilus not found. Please Install Python-Nautilus for using IMDB movie displayer"
+   }
